20bf90a7bf11afdcf5add11a836af7a0478e7599
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2023-09-29 23:27:19 -0500

    correcion de un import

--- proyecto1-main/src/main/java/uniandes/edu/co/proyecto/repository/UsuarioRepository.java
+++ proyecto1-main/src/main/java/uniandes/edu/co/proyecto/repository/UsuarioRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import uniandes.edu.co.proyecto.model.Usuario;
-import uniandes.edu.co.proyecto.model.PrimaryKeys.UsuarioPK;
+import uniandes.edu.co.proyecto.model.primaryKeys.UsuarioPK;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UsuarioPK> {
 
@@ -23,13 +23,17 @@ public interface UsuarioRepository extends JpaRepository<Usuario, UsuarioPK> {
     @Modifying
     @Transactional
     @Query(value = "INSERT INTO usuarios (num_doc, tipo_doc, tipo_id, nombre, email, password) VALUES (:numDoc, :tipoDoc, :tipoId, :nombre, :email, :password)", nativeQuery = true)
-    void insertarUsuario(@Param("numDoc") Integer numDoc, @Param("tipoDoc") String tipoDoc, @Param("tipoId") Integer tipoId, @Param("nombre") String nombre, @Param("email") String email, @Param("password") String password);
+    void insertarUsuario(@Param("numDoc") Integer numDoc, @Param("tipoDoc") String tipoDoc,
+            @Param("tipoId") Integer tipoId, @Param("nombre") String nombre, @Param("email") String email,
+            @Param("password") String password);
 
     // Método para actualizar un usuario
     @Modifying
     @Transactional
     @Query(value = "UPDATE usuarios SET tipo_id = :tipoId, nombre = :nombre, email = :email, password = :password WHERE num_doc = :numDoc AND tipo_doc = :tipoDoc", nativeQuery = true)
-    void actualizarUsuario(@Param("numDoc") Integer numDoc, @Param("tipoDoc") String tipoDoc, @Param("tipoId") Integer tipoId, @Param("nombre") String nombre, @Param("email") String email, @Param("password") String password);
+    void actualizarUsuario(@Param("numDoc") Integer numDoc, @Param("tipoDoc") String tipoDoc,
+            @Param("tipoId") Integer tipoId, @Param("nombre") String nombre, @Param("email") String email,
+            @Param("password") String password);
 
     // Método para eliminar un usuario por su id (clave primaria compuesta)
     @Modifying
