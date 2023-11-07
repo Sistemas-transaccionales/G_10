645c4489552133a2b6d12918a0ca2ad55a897e79
lauracalderon1 <111077399+lauracalderon1@users.noreply.github.com>
2023-11-07 15:47:00 -0500

    req4

--- proyecto1-main/src/main/java/uniandes/edu/co/proyecto/controller/requerimientos/Req4Controller.java
+++ proyecto1-main/src/main/java/uniandes/edu/co/proyecto/controller/requerimientos/Req4Controller.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import uniandes.edu.co.proyecto.model.requerimientos.Requerimiento4;
 import uniandes.edu.co.proyecto.repository.requerimientos.Req4Repository;
@@ -17,21 +18,18 @@ public class Req4Controller {
     @Autowired
     private Req4Repository req4Repository;
 
-    @GetMapping("/formularioRequerimiento4")
-    public String mostrarFormulario() {
-        return "formularioRequerimiento4";
-    }
+    @GetMapping("/req4_form")
+public String req4Form() {
+    return "req4_form";
+}
 
-    @GetMapping("/requerimiento4")
-    public String requerimiento4(
-            @RequestParam BigDecimal costoMinimo,
-            @RequestParam BigDecimal costoMaximo,
-            @RequestParam int capacidadMinima,
-            @RequestParam int capacidadMaxima,
-            Model model
-    ) {
-        Collection<Requerimiento4> resultados = req4Repository.serviciosFiltrados(costoMinimo, costoMaximo, capacidadMinima, capacidadMaxima);
-        model.addAttribute("resultados", resultados);
-        return "requerimiento4";
-    }
+@PostMapping("/requerimiento4")
+public String requerimiento4(@RequestParam BigDecimal costoMinimo,
+        @RequestParam BigDecimal costoMaximo, @RequestParam int capacidadMinima, @RequestParam int capacidadMaxima, Model model) {
+    Collection<Requerimiento4> resultados = req4Repository.serviciosFiltrados(costoMinimo, costoMaximo, capacidadMinima, capacidadMaxima);
+    model.addAttribute("resultados", resultados);
+    return "requerimiento4";
+}
+
+    
 }
