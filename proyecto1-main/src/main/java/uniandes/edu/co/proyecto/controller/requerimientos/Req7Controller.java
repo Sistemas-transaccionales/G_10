4eef1cfddaf894932d39c6a20575cd84f0820767
lauracalderon1 <111077399+lauracalderon1@users.noreply.github.com>
2023-11-06 23:05:14 -0500

    Requerimiento7

--- proyecto1-main/src/main/java/uniandes/edu/co/proyecto/controller/requerimientos/Req7Controller.java
+++ proyecto1-main/src/main/java/uniandes/edu/co/proyecto/controller/requerimientos/Req7Controller.java
@@ -5,19 +5,20 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import uniandes.edu.co.proyecto.model.Usuario;
+import org.springframework.stereotype.Controller;
+import uniandes.edu.co.proyecto.model.requerimientos.Requerimiento7;
 import uniandes.edu.co.proyecto.repository.requerimientos.Req7Repository;
 
+@Controller
 public class Req7Controller {
 
     @Autowired
     private Req7Repository req7Repository;
 
     @GetMapping("/requerimiento7")
-    public String getrequerimiento7(Model model) {
-        model.addAttribute("clientesBuenos", req7Repository.mostrarusuarios()); // Usar "clientesBuenos" en el modelo
+    public String requerimiento7(Model model) {
+        Collection<Requerimiento7> datosClientes = req7Repository.clientesBuenos();
+        model.addAttribute("datosClientes", datosClientes);
         return "requerimiento7";
     }
-
 }
--- proyecto1-main/src/main/java/uniandes/edu/co/proyecto/repository/requerimientos/Req7Repository.java
+++ proyecto1-main/src/main/java/uniandes/edu/co/proyecto/repository/requerimientos/Req7Repository.java
@@ -5,16 +5,25 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import uniandes.edu.co.proyecto.model.Usuario;
-import uniandes.edu.co.proyecto.model.primaryKeys.UsuarioPK;
+import uniandes.edu.co.proyecto.model.requerimientos.Requerimiento7;
 
-public interface Req7Repository extends JpaRepository<Usuario, UsuarioPK> {
+public interface Req7Repository extends JpaRepository<Requerimiento7, String> {
 
-    /*
-     * MOSTRAR LOS 20 SERVICIOS MÁS POPULARES.
-     * Los que fueron más consumidos en un período de tiempo dado.
-     */
-    @Query(value = "SELECT * FROM usuarios", nativeQuery = true)
-    Collection<Usuario> mostrarusuarios();
-
+    @Query(value = "WITH DatosCliente AS (" +
+            "SELECT " +
+            "usuarios.num_doc AS num_documento, " +
+            "usuarios.nombre AS clientes_buenos, " +
+            "SUM(rh.fecha_salida - rh.fecha_entrada) AS dias_hospedado, " +
+            "SUM(COALESCE(rh.costo, 0) + COALESCE(rs.costo, 0)) AS costo_total " +
+            "FROM usuarios " +
+            "LEFT JOIN reservas_habitacion rh ON usuarios.num_doc = rh.num_doc " +
+            "LEFT JOIN reservas_servicio rs ON usuarios.num_doc = rs.id_consumidor " +
+            "WHERE EXTRACT(YEAR FROM rh.fecha_salida) = 2023 OR EXTRACT(YEAR FROM rs.fecha) = 2023 " +
+            "GROUP BY usuarios.num_doc, usuarios.nombre" +
+            ")" +
+            "SELECT num_documento, clientes_buenos, dias_hospedado, costo_total " +
+            "FROM DatosCliente " +
+            "WHERE costo_total > 15000000 OR dias_hospedado > 14 " +
+            "ORDER BY costo_total DESC", nativeQuery = true)
+    Collection<Requerimiento7> clientesBuenos();
 }
