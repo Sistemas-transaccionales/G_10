a6506db224ce2082566cf19dd87f30eede5d8de2
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2023-12-03 18:15:59 -0500

    crud tipo habitacion

--- proyecto3-main/src/main/java/com/example/demo/controller/TipoHabitacionController.java
+++ proyecto3-main/src/main/java/com/example/demo/controller/TipoHabitacionController.java
@@ -8,10 +8,12 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.GroupOperation;
 import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.modelo.Habitacion;
@@ -79,26 +81,64 @@ public class TipoHabitacionController {
                 return "redirect:/tiposHabitaciones";
         }
 
-        @GetMapping("/startActualizarTipoHabitacion")
-        public String mostrarFormularioActualizar(Model model) {
-                model.addAttribute("tipoHabitacion", new TipoHabitacion());
-                return "actualizarTipoHabitacionForm";
+        @GetMapping("/tipoHabitacion/{tipo}/edit")
+        public String mostrarFormularioActualizar(@PathVariable("tipo") String tipo, Model model) {
+
+                GroupOperation groupOperation = Aggregation.group("tipo.tipo")
+                                .first("tipo.costo_por_noche").as("costo_por_noche")
+                                .first("tipo.capacidad").as("capacidad")
+                                .first("tipo.dotaciones").as("dotaciones")
+                                .push("_id").as("habitaciones");
+
+                Aggregation aggregation = Aggregation.newAggregation(groupOperation,
+                                Aggregation.match(Criteria.where("_id").is(tipo)),
+                                Aggregation.project().andInclude("dotaciones", "costo_por_noche", "capacidad",
+                                                "habitaciones").and("_id").as("tipo"));
+
+                List<TipoHabitacion> tiposHabitaciones = mongoTemplate
+                                .aggregate(aggregation, "habitaciones", TipoHabitacion.class).getMappedResults();
+
+                System.out.println("TIPO HABITACION");
+                TipoHabitacion tipoHabitacion = tiposHabitaciones.get(0);
+                System.out.println(tipoHabitacion);
+
+                model.addAttribute("tipoHabitacion", tipoHabitacion);
+
+                return "editarTipoHabitacion";
         }
 
-        @PostMapping("/actualizarTipoHabitacion")
-        public String actualizarTipoHabitacion(@ModelAttribute("tipoHabitacion") TipoHabitacion tipoHabitacion) {
+        @PostMapping("/tipoHabitacion/{tipo}/edit/save")
+        public String actualizarTipoHabitacion(@PathVariable("tipo") String tipo,
+                        @ModelAttribute("tipoHabitacion") TipoHabitacion tipoHabitacion) {
 
                 tipoHabitacion.getDotaciones().removeIf(String::isEmpty);
 
                 List<Habitacion> habitaciones = habitacionRepository
                                 .findByTipoTipo(tipoHabitacion.getTipo());
 
-                TipoHabitacionEmbedded tipoHabitacionEmbedded = new TipoHabitacionEmbedded(tipoHabitacion.getTipo(),
+                TipoHabitacionEmbedded tipoHabitacionEmbedded = new TipoHabitacionEmbedded(tipo,
                                 tipoHabitacion.getCosto_por_noche(), tipoHabitacion.getCapacidad(),
                                 tipoHabitacion.getDotaciones());
 
                 for (Habitacion habitacion : habitaciones) {
                         habitacion.setTipo(tipoHabitacionEmbedded);
+                        habitacionRepository.save(habitacion);
+                }
+
+                return "redirect:/tiposHabitaciones";
+        }
+
+        @GetMapping("/tipoHabitacion/{tipo}/delete")
+        public String eliminarTipoHabitacion(@PathVariable("tipo") String tipo) {
+
+                List<Habitacion> habitaciones = habitacionRepository
+                                .findByTipoTipo(tipo);
+
+                TipoHabitacionEmbedded tipoHabitacionNulo = new TipoHabitacionEmbedded("Nulo", 0, 0,
+                                new ArrayList<String>());
+
+                for (Habitacion habitacion : habitaciones) {
+                        habitacion.setTipo(tipoHabitacionNulo);
                         habitacionRepository.save(habitacion);
                 }
 
