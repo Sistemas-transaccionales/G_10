bd544de66bf87fd2c84f04daa016e6b6beed7a3c
DavidTobonIBIO <d.tobonm2@uniandes.edu.co>
2023-12-03 09:39:09 -0500

    start reqs david

---
+++ proyecto3-main/src/main/java/com/example/demo/controller/TipoHabitacionController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.modelo.Habitacion;
+import com.example.demo.modelo.TipoHabitacion;
+import com.example.demo.repositorio.HabitacionRepository;
+import com.example.demo.repositorio.TipoHabitacionRepository;
+
+@Controller
+public class TipoHabitacionController {
+
+    @Autowired
+    private TipoHabitacionRepository tipoHabitacionRepository;
+
+    @Autowired
+    private HabitacionRepository habitacionRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @GetMapping("/tiposHabitaciones")
+    public String getTipoHabitaciones(Model model) {
+
+        GroupOperation groupOperation = GroupOperation.groupOperation(group("tipo.tipo")
+                .first("tipo.costo_por_noche").as("costo_por_noche")
+                .first("tipo.capacidad").as("capacidad")
+                .first("tipo.dotaciones").as("dotaciones")
+                .push("_id").as("habitaciones"));
+
+        ProjectionOperation projectOperation = ProjectionOperation.ProjectionOperationBuilder(project()
+                .andExclude("_id")
+                .andInclude("tipo", "dotaciones", "costo_por_noche", "capacidad", "habitaciones"));
+
+        Aggregation aggregation = Aggregation.newAggregation(groupOperation, projectOperation);
+
+        List<TipoHabitacion> tiposHabitacion = mongoTemplate
+                .aggregate(aggregation, "habitaciones", TipoHabitacion.class).getMappedResults();
+        model.addAttribute("tiposHabitaciones", tiposHabitacion);
+
+        return "resultadosTiposHabitaciones";
+    }
+
+}
---
+++ proyecto3-main/src/main/java/com/example/demo/modelo/TipoHabitacion.java
@@ -0,0 +1,126 @@
+package com.example.demo.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "tipo_habitaciones")
+public class TipoHabitacion {
+
+    @Id
+    private String tipo;
+
+    @Field("costo_por_noche")
+    private int costo_por_noche;
+
+    @Field("capacidad")
+    private int capacidad;
+
+    @Field("dotaciones")
+    private List<String> dotaciones;
+
+    @DBRef
+    private List<Habitacion> habitaciones;
+
+    public TipoHabitacion() {
+    }
+
+    public TipoHabitacion(String tipo, int costo_por_noche, int capacidad, List<String> dotaciones,
+            List<Habitacion> habitaciones) {
+        this.tipo = tipo;
+        this.costo_por_noche = costo_por_noche;
+        this.capacidad = capacidad;
+        this.dotaciones = dotaciones;
+        this.habitaciones = habitaciones;
+    }
+
+    public TipoHabitacion(String tipo, int costo_por_noche, int capacidad, List<String> dotaciones) {
+        this.tipo = tipo;
+        this.costo_por_noche = costo_por_noche;
+        this.capacidad = capacidad;
+        this.dotaciones = dotaciones;
+        this.habitaciones = new ArrayList<Habitacion>();
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the costo_por_noche
+     */
+    public int getCosto_por_noche() {
+        return costo_por_noche;
+    }
+
+    /**
+     * @param costo_por_noche the costo_por_noche to set
+     */
+    public void setCosto_por_noche(int costo_por_noche) {
+        this.costo_por_noche = costo_por_noche;
+    }
+
+    /**
+     * @return the capacidad
+     */
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    /**
+     * @param capacidad the capacidad to set
+     */
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    /**
+     * @return the dotaciones
+     */
+    public List<String> getDotaciones() {
+        return dotaciones;
+    }
+
+    /**
+     * @param dotaciones the dotaciones to set
+     */
+    public void setDotaciones(List<String> dotaciones) {
+        this.dotaciones = dotaciones;
+    }
+
+    /**
+     * @return the habitaciones
+     */
+    public List<Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+
+    /**
+     * @param habitaciones the habitaciones to set
+     */
+    public void setHabitaciones(List<Habitacion> habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
+    /**
+     * @param habitacion the habitacion to add
+     */
+    public void addHabitacion(Habitacion habitacion) {
+        this.habitaciones.add(habitacion);
+    }
+
+}
